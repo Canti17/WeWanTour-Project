d21b8bc698ef15e99895b5e7b7500144789851f5
Andrea Franceschi <franceschi.1709888@studenti.uniroma1.it>
2020-04-07 01:55:44 +0200

    Add transport form with initial style formatting

--- WeWanTour/app/src/main/java/com/example/wewantour9/MainActivity.java
+++ WeWanTour/app/src/main/java/com/example/wewantour9/MainActivity.java
@@ -1,89 +1,34 @@
 package com.example.wewantour9;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button send;
-
-
-    private FirebaseDatabase database;
-    private DatabaseReference ref;
-
-    EditText name, description, n_clients;
-    Tour tour1;
-    Prova prova1;
-    int id=0;
-
-
-
-
-    ArrayList<Tour> lista = new ArrayList<Tour>();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        send= (Button) findViewById(R.id.button);
-
-        name=findViewById(R.id.nameTour);
-        description=findViewById(R.id.description);
-        n_clients=findViewById(R.id.numb_clients);
-
-
-        ref= database.getInstance().getReference("TOUR");
-
-
-
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    id= (int) dataSnapshot.getChildrenCount();
-                }else{
-                    ///
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        send.setOnClickListener(new View.OnClickListener() {
+/*
+        Button btn1 = (Button)findViewById(R.id.add_tour);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tour1= new Tour(name.getText().toString(),description.getText().toString(),n_clients.getText().toString());
-                ref.child(String.valueOf(id)).setValue(tour1);
-
-                DatabaseReference ref1= database.getInstance().getReference("PROVA");
-                lista.add(tour1);
-                prova1 = new Prova(lista);
-                ref1.child((String.valueOf(tour1.getName()))).setValue(prova1);
-
-
+                startActivity(new Intent(MainActivity.this, AddTour.class));
             }
         });
-
+*/
+        Button btn2 = (Button)findViewById(R.id.add_transport);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, add_transport.class));
+            }
+        });
 
     }
 }
