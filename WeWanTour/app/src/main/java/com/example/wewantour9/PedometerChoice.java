807f7b185ab6664a70836e10b03bec2faf6d7d88
Daniele Cantisani <cantisani.1707633@studenti.uniroma1.it>
2020-12-14 19:24:42 +0100

    Update Design Pedometer
    Sempre Onore a Rick

--- WeWanTour/app/src/main/java/com/example/wewantour9/PedometerChoice.java
+++ WeWanTour/app/src/main/java/com/example/wewantour9/PedometerChoice.java
@@ -10,14 +10,20 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class PedometerChoice extends AppCompatActivity {
 
     Toolbar toolbar;
     Button startbutton;
+    private TextInputEditText height_p;
+    private TextInputEditText weight_p;
+
+    private Spinner spinner;
 
     @Override
     public void onBackPressed() {
@@ -30,9 +36,17 @@ public class PedometerChoice extends AppCompatActivity {
         setContentView(R.layout.activity_pedometer_choice);
         toolbar = findViewById(R.id.toolbar);
         startbutton = findViewById(R.id.startbutton);
+        spinner = findViewById(R.id.spinner);
+
+        height_p = findViewById(R.id.height_field);
+        weight_p = findViewById(R.id.weight_field);
+
+        final String height = height_p.getText().toString();
+        final String weight = weight_p.getText().toString();
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Pedometro");
 
 /*GESTIONE ACTIVITIES
         int data = DataHolder.getInstance().getData();
@@ -43,7 +57,11 @@ public class PedometerChoice extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 DataHolder.getInstance().setData(1);
-                startActivity(new Intent(PedometerChoice.this, PedometerRun.class));
+                Intent intentrun = new Intent(PedometerChoice.this, PedometerRun.class);
+                intentrun.putExtra("Height", height);
+                intentrun.putExtra("Weight", weight);
+
+                startActivity(intentrun);
                 finish();
 
             }
