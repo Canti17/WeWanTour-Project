5d692720b804bb0d9f6121c116642abc753738ec
Andrea Franceschi <franceschi.1709888@studenti.uniroma1.it>
2021-01-15 20:05:19 +0100

    Server end without security

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
-
+import json
+from pathlib import Path
+import os.path
 
 application = Flask(__name__)
 api = Api(application)
@@ -8,6 +10,22 @@ api = Api(application)
 
 parser = reqparse.RequestParser()
 another_parser = reqparse.RequestParser()
+
+#Read all the reviews from a persistent file
+def retrieve_reviews():
+    buffer_list = []
+    flag = False
+    if os.path.isfile('reviews.txt') :
+        flag = True
+    else:
+        flag = False
+    if(flag):
+        with open('reviews.txt', 'r') as reviews_db:
+            buffer_list = json.load(reviews_db)
+    print(buffer_list)
+    return buffer_list
+
+reviews_list = retrieve_reviews()     #list of the form [[review], [review], ...] each review has the form [user_email, tour_id, review_score, review_message]
 
 class Server(Resource):
 
@@ -18,8 +36,10 @@ class Server(Resource):
 
         tourid = args['tourid'] #LA RICHIESTA CLIENT AVRA PARAMETRO TOURID
 
+        average_rating, reviews_number = get_number_rating(tourid)
+        print(average_rating, reviews_number)
         
-        return {'msg': "GET", 'error': False}
+        return {'msg': "GET", 'error': False, 'rating': average_rating, 'number': reviews_number}
 
 
 
@@ -29,9 +49,13 @@ class Server(Resource):
         parser.add_argument('rate', type=float, required = True)
         args = parser.parse_args()
 
+
         email = args['email'] #LA RICHIESTA CLIENT AVRA PARAMETRO EMAIL
         tourid = args['tourid']  #IL TOUR ID DELLA RICHIESTA POST
         rate = args['rate']#IL VOTO DATO DAL CLIENT
+        comment = request.get_json(force=True)['Comment']#IL COMMENTO DATO DAL CLIENT
+
+        new_review(email, tourid, rate, comment)
 
         return {'msg': "POST", 'error': False}
 
@@ -44,15 +68,49 @@ class Server(Resource):
 
         tourid = args['tourid'] #LA RICHIESTA CLIENT AVRA PARAMETRO TOURID
 
+        review_tour_delete(tourid)
+        print(reviews_list)
         
         return {'msg': "DELETE", 'error': False}
 
-            
-        
+
+#Add a review (done with the post)
+def new_review (email, tourid, rate, message):
+    buffer_list = [email, tourid, rate, message]
+    for i in range(len(reviews_list)):
+        if ((reviews_list[i][0] == buffer_list[0]) and (reviews_list[i][1] == buffer_list[1])):
+            del reviews_list[i]
+    reviews_list.append(buffer_list)
+    print(reviews_list)
+    store_reviews();
+
+#Return a rating and the number of reviews for a tour (done with the get)
+def get_number_rating(tourid):
+    sum_of_reviews_rating = 0
+    total_number_of_reviews = 0
+    for i in range(len(reviews_list)):
+        if (reviews_list[i][1] == tourid):
+            sum_of_reviews_rating += reviews_list[i][2]
+            total_number_of_reviews += 1
+    average_rating = sum_of_reviews_rating/total_number_of_reviews
+    return average_rating, total_number_of_reviews
+
+#Delete all the reviews of one tour (done with the delete)
+def review_tour_delete(tourid):
+    for i in range(len(reviews_list)):
+        if (reviews_list[i][1] == tourid):
+            del reviews_list[i]
+    print(reviews_list)
+    store_reviews();
+
+#Write all the reviews on a file to get them persistent
+def store_reviews():
+    with open('reviews.txt', 'w') as reviews_db:
+        json.dump(reviews_list, reviews_db)
+
 
 api.add_resource(Server, '/')
 
 if __name__ == '__main__':
     print('starting api....')
     application.run(host='0.0.0.0')
-    
